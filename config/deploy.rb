ebe76402b1bd898c08b891905e407c5eb45f96d7
Sean Lingren <srlingren@gmail.com>
2015-09-13 14:36:35 -0700

    Finished Capistrano Setup, Deploying to Digital Ocean.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,51 +1,38 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-# Define the name of the application
 set :application, 'churner'
-
-# Define where can Capistrano access the source repository
-# set :repo_url, 'https://github.com/Lingrino/churner.git'
-set :scm, :git
 set :repo_url, 'https://github.com/Lingrino/churner.git'
 
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Define where to put your application code
+# Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, "/home/deployer/apps/churner"
 
-set :pty, true
+# Default value for :scm is :git
+# set :scm, :git
 
-set :format, :pretty
+# Default value for :format is :pretty
+# set :format, :pretty
 
-# Set the post-deployment instructions here.
-# Once the deployment is complete, Capistrano
-# will begin performing them as described.
-# To learn more about creating tasks,
-# check out:
-# http://capistranorb.com/
+# Default value for :log_level is :debug
+# set :log_level, :debug
 
-# namespace: deploy do
+# Default value for :pty is false
+# set :pty, true
 
-#   desc 'Restart application'
-#   task :restart do
-#     on roles(:app), in: :sequence, wait: 5 do
-#       # Your restart mechanism here, for example:
-#       execute :touch, release_path.join('tmp/restart.txt')
-#     end
-#   end
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
-#   after :publishing, :restart
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
-#   after :restart, :clear_cache do
-#     on roles(:web), in: :groups, limit: 3, wait: 10 do
-#       # Here we can do anything such as:
-#       # within release_path do
-#       #   execute :rake, 'cache:clear'
-#       # end
-#     end
-#   end
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-# end
+# Default value for keep_releases is 5
+# set :keep_releases, 5
 
 namespace :deploy do
 
