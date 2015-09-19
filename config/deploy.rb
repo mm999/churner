25f70680953317d3ac847485e725745757e62bca
Sean Lingren <srlingren@gmail.com>
2015-09-19 15:24:34 -0700

    Braintree production mode

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,13 +38,6 @@ namespace :deploy do
 
 	after :publishing, :restart
 
-	# desc 'Restart application'
-	# task :restart do
-	#   on roles(:app), in: :sequence, wait: 5 do
-	#   	run "passenger-config restart-app $(pwd)"
-	#   end
-	# end
-
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
