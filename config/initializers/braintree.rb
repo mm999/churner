ce239f3db3bd0b3f440c3fc4b183d59aae9e6f66
Sean Lingren <srlingren@gmail.com>
2015-09-22 19:28:01 -0700

    Small config changes

--- config/initializers/braintree.rb
+++ config/initializers/braintree.rb
@@ -2,3 +2,4 @@ Braintree::Configuration.environment = Rails.application.secrets.braintree_envir
 Braintree::Configuration.merchant_id = Rails.application.secrets.braintree_merchant_id
 Braintree::Configuration.public_key = Rails.application.secrets.braintree_public_key
 Braintree::Configuration.private_key = Rails.application.secrets.braintree_private_key
+
